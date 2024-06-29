25242a040c3c1780c54c553cddbabd71cfdd1b40
mabdullahjs <mabdullah2037@outlook.com>
2024-06-29 15:42:11 +0500

    js started

---
+++ Javascript/01-Introduction/app.js
@@ -0,0 +1,90 @@
+// console.log("Hello world!");
+
+// console.log(10 / 5);
+
+// console.log("hello Javascript")
+
+
+// console.log(2 + 2);
+// console.log("2" + "2");
+
+
+// Data types
+
+//string "324skdla;skd#@$@#$"
+//number 4234.23423
+//boolean true | false
+
+
+// console.log(2 + 2);
+
+//concatenation
+// console.log("2a" + "2b");
+
+// console.log("My name is Muhammad Abdullah")
+
+// console.log(true)
+// console.log(20)
+// console.log("20")
+
+
+
+// alert("Hello world!");
+
+// alert(2 + 2);
+
+
+// console.log('abc');
+
+
+//variables
+
+// var city = "karachi";
+
+// var username = 'abdullah';
+// var age = 20;
+// var isLoggedIn = true;
+
+// console.log(username , age , isLoggedIn);
+
+
+
+// var num1 = "20";
+// var num2 = "30";
+// console.log(num1 + num2);
+
+
+
+// var 123username = 'abdullah'; xxxx
+// var %username123 = 'abdullah'; xxxx 
+// var $_username123 = 'abdullah'; 
+// // var full Name = 'abdullah';xxxx 
+// var fullName = 'abdullah';
+
+// console.log(fullName);
+
+
+// // console.log(dsadsjajdsjkh) xxx
+// console.log($_username123);
+
+// var username;
+// console.log(username);
+
+
+// var city = 'karachi'; //decleration
+// console.log(city);
+
+// var city = 'islamabad'; //redeclare
+// console.log(city);
+
+// city = 'lahore'; //reassign
+// console.log(city);
+
+
+
+// var weight = 29;
+// console.log(weight + 30)
+
+
+
+var fullname = 'abdullah'
