43a7113006e12acae559ac5c47ecaef58cae75ac
mabdullahjs <mabdullah2037@outlook.com>
2024-07-06 15:52:40 +0500

    if else statement

---
+++ Javascript/03-If-Else-Statement/app.js
@@ -0,0 +1,205 @@
+// console.log('hello world!');
+
+// comparision operator
+// >
+// <
+// >=
+// <=
+// ==
+// ===
+// !==
+// !=
+
+var num1 = 10;
+var num2 = 5;
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 === num2);
+// console.log(num1 != num2); //false
+// console.log(num1 !== num2); //true
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+
+
+//logical operator
+
+// || eng: orr  urdu:yaa
+// && eng: and urdu: orr
+// !
+
+// console.log(num1 == num2 || num1 < num2 || num1 === num2) //true
+
+
+// || koi aik bhi condition agar true hogaye to true return krdaiga.. sab conditions agar false hongi tabhi false ayega..
+
+// && koi aik bhi condition agar false hogaye to false return krdaiga.. sab conditions agar true hongi tabhi true ayega..
+
+
+// console.log(10 > 5 && 5 < 10)
+
+// console.log(10 > 5 && 10 === 5 || 4 > 6 && 5 < 5);
+
+//true && false || false && false
+//false || false && false
+//false && false
+//false
+
+
+
+
+// console.log(8 % 2 === 0 && 10 <= 10 || 5 + 6 !== 11 && 8 >= 8 || 9 != "9")
+
+// true && true || false && true || false
+//true || false && true || false
+// true && true || false
+// true || false
+// true
+
+
+
+
+
+
+
+
+
+
+// console.log(
+//     (15 % 4 === 3 && 20 > 15 || 7 * 3 === 21 && 12 - 6 !== 6) &&
+//     (8 % 3 === 2 || 25 <= 30 && 5 !== 5) ||
+//     (100 / 10 === 10 && 9 == "9" || 14 + 1 !== 15 && 7 <= 7)
+// );
+// console.log(
+//     false && false || true
+// );
+
+
+// (15 % 4 === 3 && 20 > 15 || 7 * 3 === 21 && 12 - 6 !== 6)
+
+// true && true || true && false
+// true || true && false
+// true && false
+//false
+
+
+// (8 % 3 === 2 || 25 <= 30 && 5 !== 5)
+
+// true || true && false
+//false
+
+// (100 / 10 === 10 && 9 == "9" || 14 + 1 !== 15 && 7 <= 7)
+
+//true
+
+// false && false || true
+
+
+
+
+
+
+
+
+
+
+
+
+//control flow
+
+// var username = 'abdullah';
+// console.log(username);
+
+var num1 = 10;
+
+//if else statement
+
+// if(num1 > 5){
+//     console.log('if chal rha ha')
+// }else{
+//     console.log('else chal rha ha')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var totalMarks = 20;
+
+// if (totalMarks >= 80) {
+//     console.log('A+ grade');
+// } else if (totalMarks >= 70) {
+//     console.log('A grade');
+// } else if (totalMarks >= 60) {
+//     console.log('B grade')
+// } else {
+//     console.log('fail');
+// }
+
+
+// console.log('console after if else')
+
+
+
+
+
+
+
+
+
+// var bankBalance = 200000
+
+// if(bankBalance > 1000000){
+//     console.log('shaadi Mubarak')
+// }else{
+//     console.log('ammi na zehr kahlia')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var bankBalance = 1000000;
+
+// if(bankBalance > 1000000){
+//     console.log('shaadi mubarak');
+// }else if(bankBalance > 800000){
+//     console.log('abbu sa baat krongi')
+// }else if(bankBalance > 500000){
+//     console.log('papa nahi maan rha')
+// }else{
+//     console.log('ammi keh rhi hain chalang maarlongi');
+// }
+
+
+
+
+
+
+
+
+
+
+
