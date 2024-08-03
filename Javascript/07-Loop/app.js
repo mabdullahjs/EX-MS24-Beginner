d0c8ac1fcf02bf66d593855b467ba5a92c4cc521
mabdullahjs <mabdullah2037@outlook.com>
2024-08-03 22:07:17 +0500

    loops completed

--- Javascript/07-Loop/app.js
+++ Javascript/07-Loop/app.js
@@ -1,5 +1,152 @@
+// var userInput = +prompt("enter a number");
 
+// console.log(userInput + " * 1 = " + userInput * 1);
+// console.log(userInput + " * 2 = " + userInput * 2);
+// console.log(userInput + " * 3 = " + userInput * 3);
+// console.log(userInput + " * 4 = " + userInput * 4);
+// console.log(userInput + " * 5 = " + userInput * 5);
+// console.log(userInput + " * 6 = " + userInput * 6);
+// console.log(userInput + " * 7 = " + userInput * 7);
+// console.log(userInput + " * 8 = " + userInput * 8);
+// console.log(userInput + " * 9 = " + userInput * 9);
+// console.log(userInput + " * 10 = " + userInput * 10);
+
+
+
+
+
+
+
+
+
+
+//for loop
+//while loop
+//do while loop
+
+
+//initialization
+//condition
+//increment
+
+// for(var i = 1; i <= 10; i++){
+//     console.log("2 * " + i + " = " + 2 * i);
+// }
+
+
+
+
+
+
+
+
+
+var div = document.querySelector("div");
+var fruits = ['apple', 'banana', 'orange', 'kiwi'];
+
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+
+// for (var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+//     div.innerHTML +=fruits[i] + "<br/>"
+// }
+
+
+
+
+// dom manipulation
+// loop ka through array render krwana
+// splice
+// push
+
+
+
+
+
+
+
+
+
+
+
+// nested loop
 
 // for(var i = 0; i < 10; i++){
-//     console.log('hello world')
+//     console.log("outer loop " , i)
+//     for(var j = 0; j < 10; j++){
+//         console.log('inner loop ' , j)
+//     }
 // }
+
+
+
+
+
+// for(var i = 1; i <= 100; i++){
+//     console.log("table of  " , i , "====")
+//     for(var j = 1; j <= 10; j++){
+//         console.log(i + " * " + j +  " = " + i * j)
+//     }
+// }
+
+
+
+
+
+// while
+// do while
+
+
+// for(var i = 10; i <= 9; i++){
+//     console.log("2 * " + i + " = " + 2 * i);
+// }
+
+
+// var i = 0;
+// while (i < 10) {
+//     console.log("2 * " + i + " = " + 2 * i);
+//     i++
+// }
+
+
+// var i = 10
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 9)
+
+
+
+
+
+// break
+// for(var i = 1; i <= 10; i++){
+//     if(i == 5){
+//         console.log('5 tracked');  
+//     }
+//     console.log(i)
+// }
+
+
+// for(var i = 1; i <= 10; i++){
+//     if(i == 5){
+//         console.log('5 tracked');
+//         break
+//     }
+//     console.log(i)
+// }
+
+
+//continue
+
+// for(var i = 1; i <= 10; i++){
+//     if(i == 5){
+//         console.log('5 tracked');
+//         continue
+//     }
+//     console.log(i)
+// }
+
