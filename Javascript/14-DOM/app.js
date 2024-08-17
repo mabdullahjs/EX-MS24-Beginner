681451348baac062a86799a6e48119cb56a18766
mabdullahjs <mabdullah2037@outlook.com>
2024-08-17 16:00:02 +0500

    objects

---
+++ Javascript/14-DOM/app.js
@@ -0,0 +1,35 @@
+// console.log('hello world');
+
+
+// const head = document.querySelector("#head-one");
+// const head = document.getElementById("head");
+// const head = document.getElementsByClassName("head");
+// const head = document.getElementsByTagName("h1");
+// const head = document.querySelectorAll("h1")[1];
+// console.log(head);
+
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].nodeType)
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[0].nodeType)
+
+// const ul = document.getElementById("ul")
+
+// console.log(ul.lastChild.nodeName);
+
+
+const div = document.querySelector("div");
+
+// div.innerHTML = `
+// <h1>hello world</h1>
+// <ul>
+//     <li>Home</li>
+//     <li>About</li>
+//     <li>Contact</li>
+// </ul>
+// `
+
+// let h1 = document.createElement("H1");
+// let h1Text = document.createTextNode("hello world");
+// h1.appendChild(h1Text);
+// div.appendChild(h1)
+// console.log(h1);
